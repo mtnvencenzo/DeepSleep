3b86776b7909606e15637078e4b209907b761c91
Ronaldo Vecchi <rvecchi@gmail.com>
2020-12-13 13:32:22 +0000

    Merged PR 213: Adding constructors to exceptions
    Adding constructors to exceptions

--- DeepSleep/ApiGatewayTimeoutException.cs
+++ DeepSleep/ApiGatewayTimeoutException.cs
@@ -1,11 +1,25 @@
 ﻿namespace DeepSleep
 {
+    using System;
+
     /// <summary>
     /// 
     /// </summary>
     /// <seealso cref="System.Exception" />
     public class ApiGatewayTimeoutException : ApiException
     {
+        /// <summary>Initializes a new instance of the <see cref="ApiGatewayTimeoutException"/> class.</summary>
+        public ApiGatewayTimeoutException() : base() { }
+
+        /// <summary>Initializes a new instance of the <see cref="ApiGatewayTimeoutException"/> class.</summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ApiGatewayTimeoutException(string message) : base(message) { }
+
+        /// <summary>Initializes a new instance of the <see cref="ApiGatewayTimeoutException"/> class.</summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ApiGatewayTimeoutException(string message, Exception innerException) : base(message, innerException) { }
+
         /// <summary>Gets the HTTP status.</summary>
         /// <value>The HTTP status.</value>
         public override int HttpStatus { get; } = 504;
--- DeepSleep/ApiNotImplementedException.cs
+++ DeepSleep/ApiNotImplementedException.cs
@@ -1,11 +1,25 @@
 ﻿namespace DeepSleep
 {
+    using System;
+
     /// <summary>
     /// 
     /// </summary>
     /// <seealso cref="System.Exception" />
     public class ApiNotImplementedException : ApiException
     {
+        /// <summary>Initializes a new instance of the <see cref="ApiNotImplementedException"/> class.</summary>
+        public ApiNotImplementedException() : base() { }
+
+        /// <summary>Initializes a new instance of the <see cref="ApiNotImplementedException"/> class.</summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ApiNotImplementedException(string message) : base(message) { }
+
+        /// <summary>Initializes a new instance of the <see cref="ApiNotImplementedException"/> class.</summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ApiNotImplementedException(string message, Exception innerException) : base(message, innerException) { }
+
         /// <summary>Gets the HTTP status.</summary>
         /// <value>The HTTP status.</value>
         public override int HttpStatus { get; } = 501;
